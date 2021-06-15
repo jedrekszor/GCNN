345a88414f86ac5a828ba775a28a0ca2020e9eb5
Michał Suliborski <msuliborski@gmail.com>
2021-06-15 21:12:13 +0200

    wad

--- src/visualize.py
+++ src/visualize.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plt
 import torch
 from torch_geometric.utils.convert import to_networkx
 
-
 def visualize(dataset):
-    for i in range(10, 16):
-        mnist = to_networkx(dataset[i])
+    mnist = to_networkx(dataset[0])
 
     dataset.y = torch.zeros(75)
     node_labels = dataset.y.numpy()
 
-    plt.figure(1, figsize=(5, 5))
-    nx.draw(mnist, cmap=plt.get_cmap('Set2'), node_color=node_labels, node_size=30, linewidths=6)
+    plt.figure(1, figsize=(8, 8))
+    nx.draw(mnist, cmap=plt.get_cmap('Set1'), node_color=node_labels, node_size=30, linewidths=6)
     plt.show()
 
 
@@ -22,7 +20,7 @@ def visualize_t(dataset):
     node_labels = dataset[0].y[list(graph.nodes)].numpy()
 
     import matplotlib.pyplot as plt
-    plt.figure(1, figsize=(14, 12))
-    nx.draw(graph, cmap=plt.get_cmap('Blues'), node_color=node_labels, node_size=25, linewidths=3)
+    plt.figure(1, figsize=(16, 16))
+    nx.draw(graph, cmap=plt.get_cmap('Set1'), node_color=node_labels, node_size=25, linewidths=3)
     plt.show()
 
